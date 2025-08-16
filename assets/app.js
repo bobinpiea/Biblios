550008ceef995209261c296e2268e72cfc5d487f
Bobin <bobinpierreandre@gmail.com>
2025-08-16 12:38:18 +0200

    Refonte de la page d’accueil avec Twig et Bootstrap
    Mise à jour des fichiers base.html.twig et index.html.twig pour intégrer un design Bootstrap, améliorer la mise en page et afficher les sections de présentation et de fonctionnalités de Biblios.

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,12 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+
+// CSS : Bootstrap d’abord, puis tes CSS
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
 import './styles/app.css';
+
+// JS : importer Bootstrap avant ton script perso
+import 'bootstrap';
+import './js/scripts.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
